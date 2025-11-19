fa8814bcfd698ac65d3defcc1ba0ff312713b5c6
AdamRival <adam.rival44@gmail.com>
2025-11-19 17:05:11 +0100

    no front

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating 5 flats"
+Flat.create!(name: "Hello", address: "Nantes", description: "hello this is a description", price_per_night: "125", number_of_guests: "225")
+Flat.create!(name: "Je suis un Flat", address: "Nantes aussi", description: "hello this is a description", price_per_night: "125", number_of_guests: "225")
+Flat.create!(name: "Chaise", address: "Paris", description: "hello this is a description", price_per_night: "125", number_of_guests: "225")
+Flat.create!(name: "J'ai vraiment pas d'idées", address: "Montpellier", description: "hello this is a description", price_per_night: "125", number_of_guests: "225")
+Flat.create!(name: "Le Lion et L'agneau", address: "Nantes", description: "hello this is a description", price_per_night: "125", number_of_guests: "225")
+
+puts "Finished! Created #{Flat.count} flats."
